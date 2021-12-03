83e24046e692084bb7a5fb2082be3085542cc97d
Damaryayo <damash98.com>
2021-12-03 09:43:07 +0200

    init

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express= require("express");
 const app =express();
 
-app.get('distance',function(req, res){
+app.get('/distance',function(req, res){
     res.send("24 meters"); //read page 60 of full stack book
 })
 
-app.get('distance2', function(req, res){
+app.get('/distance2', function(req, res){
     res.send(`${2*50}`);
 })
 
 app.listen(3000, function(){
-    console.log('server is listening on port 3000')
+    console.log('server is listening on port 3000');
     // listen method of app starts up the server and has it listen port 3000 of the Pi.
 })
