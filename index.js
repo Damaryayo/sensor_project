a0ab355158f3c90c86fdecf8126cb63712874c6b
Damaryayo <damash98@gmail.com>
2021-12-03 09:46:06 +0200

    init

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express= require("express");
 const app =express();
 
+
+app.get('/', function(req, res){
+    res.send('welcome to my WOT');
+})
+
 app.get('/distance',function(req, res){
     res.send("24 meters"); //read page 60 of full stack book
 })
 
 app.get('/distance2', function(req, res){
-    res.send(`${2*50}`);
+    res.send(`${2*50} meters`);
 })
 
 app.listen(3000, function(){
